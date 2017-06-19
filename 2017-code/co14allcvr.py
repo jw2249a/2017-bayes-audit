96754aeef58d6ca2f48a959dd6394246a13e538b
Neal McBurnett <neal@mcburnett.org>
2017-06-19 14:30:28 -0600

    Add co14allcvr, fix comments on co14noncvr

--- 2017-code/co14allcvr.py
+++ 2017-code/co14allcvr.py
@@ -1,4 +1,4 @@
-# co14noncvr
+# co14allcvr
 # Ronald L. Rivest, Neal McBurnett
 
 """
@@ -12,17 +12,17 @@ def election_structure(e):
     e.cids = ["STRAT"]
 
     # three paper ballot collections
-    e.pbcids = ["CCVR", "CNONCVR"]
+    e.pbcids = ["CCVR", "CCVR2"]
 
     e.collection_type["CCVR"] = "CVR"
-    e.collection_type["CNONCVR"] = "noCVR"
+    e.collection_type["CCVR2"] = "CVR"
 
     # Structure
     for cid in e.cids:
         for pbcid in e.pbcids:
             e.rel[(cid, pbcid)] = False    # default
     e.rel[("STRAT", "CCVR")] = True
-    e.rel[("STRAT", "CNONCVR")] = True          
+    e.rel[("STRAT", "CCVR2")] = True          
 
     e.vvids["STRAT"] = ["0", "1"]              # valid votes for each contest
 
@@ -34,18 +34,18 @@ def election_structure(e):
                 if e.rel[(cid, pbcid)]]):
             e.ivids[cid].append("noCVR")
 
-NONCVR_P = .14
-CVR_P = 1 - NONCVR_P
+CVR2_P = .14
+CVR_P = 1 - CVR2_P
 
 def election_data(e):
     """
-    A stratified election with 100000 ballots, where 1 is the winner, with a margin of 2%,
+    An election with 100000 ballots, where 1 is the winner, with a margin of 2%,
     and 10% are Invalid (undervote/overvote/residual).
-    14% noCVR
+    Two counties, with 86% / 14%.
     """
 
     e.n["CCVR"] = int(10000 * CVR_P)
-    e.n["CNONCVR"] = int(10000 * NONCVR_P)
+    e.n["CCVR2"] = int(10000 * CVR2_P)
 
     # e.t = vote totals for each cid pbcid vid combo
     for cid in e.cids:
@@ -53,11 +53,11 @@ def election_data(e):
             for vid in e.vids[cid]:
                 e.t[(cid, pbcid, vid)] = 0
     e.t[("STRAT", "CCVR", "1")] = int(46000 * .1 * CVR_P)
-    e.t[("STRAT", "CNONCVR", "1")] = int(46000 * .1 * NONCVR_P)
+    e.t[("STRAT", "CCVR2", "1")] = int(46000 * .1 * CVR2_P)
     e.t[("STRAT", "CCVR", "Invalid")] = int(10000 * .1 * CVR_P)
     e.t[("STRAT", "CCVR", "0")] = int(44000 * .1 * CVR_P)
-    e.t[("STRAT", "CNONCVR", "0")] = int(44000 * .1 * NONCVR_P)
-    e.t[("STRAT", "CNONCVR", "Invalid")] = int(10000 * .1 * NONCVR_P)
+    e.t[("STRAT", "CCVR2", "0")] = int(44000 * .1 * CVR2_P)
+    e.t[("STRAT", "CCVR2", "Invalid")] = int(10000 * .1 * CVR2_P)
     
     # e.ro = reported outcomes for each cid (all correct here)
     e.ro["STRAT"] = "1"                         
@@ -65,8 +65,8 @@ def election_data(e):
 def audit_parameters(e):
 
     e.risk_limit["STRAT"] = 0.05               # risk limit by contest
-    e.audit_rate["CCVR"] = 50    # max rate/stage for auditing ballots by pbcid
-    e.audit_rate["CNONCVR"] = 25 
+    e.audit_rate["CCVR"] = 20    # max rate/stage for auditing ballots by pbcid
+    e.audit_rate["CCVR2"] = 10 
 
     # Each contest status should be "Auditing" or "Just Watching"
     e.contest_status["STRAT"] = "Auditing"
