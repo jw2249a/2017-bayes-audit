8c942ec6e2238803c22755defdc9668be51c3362
ron-rivest <rivest@mit.edu>
2017-06-30 16:04:26 -0400

    trying out different election parameters

--- 2017-code/elections/ex1/structure.js
+++ 2017-code/elections/ex1/structure.js
@@ -7,11 +7,24 @@
   "__comment": "five contests",
   "cids": ["I", "C1", "C2", "C3", "F23"],
 
+  "__comment": "valid selections for each contest",
+  "selids": {"I": {"0": true,
+                   "1": true},
+             "C1": {"0": true,
+                    "1": true},
+             "C2": {"0": true,
+                    "1": true},
+             "C3": {"0": true,
+                    "1": true},
+             "F23": {"0": true,
+                     "1": true}}
+
   "__comment": "three paper ballot collections",
   "pbcids": ["PBC1",
              "PBC2",
              "PBC3"],
 
+  "__comment": "type of each collection: CVR or noCVR",
   "collection_type":  {"PBC1": "CVR",
                        "PBC2": "CVR",
                        "PBC3": "CVR"},
@@ -27,16 +40,5 @@
       "F23": {"PBC2": true,
               "PBC3": true}},
 
-  "__comment": "valid selections for each contest",
-  "selids": {"I": {"0": true,
-                   "1": true},
-             "C1": {"0": true,
-                    "1": true},
-             "C2": {"0": true,
-                    "1": true},
-             "C3": {"0": true,
-                    "1": true},
-             "F23": {"0": true,
-                     "1": true}}
 }            
             
