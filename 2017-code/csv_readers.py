c825acd1bd1db85ed0036a2c478f144007678faa
ron-rivest <rivest@mit.edu>
2017-07-27 10:26:24 -0700

    Update csv_readers.py to fix bugs.

--- 2017-code/csv_readers.py
+++ 2017-code/csv_readers.py
@@ -1,17 +1,18 @@
 # csv_readers.py
 # Ronald L. Rivest and Karim Husayn Karimi
-# July 7, 2017
+# July 27, 2017
 # python3
 
 """
-Code to read the various files that multi.py uses from their CSV
-formats, and put the results into the Election data structure.
+Code to read the various files that multi.py uses from their CSV formats.
 """
 
 """
 All CSV files have a single header line, giving the column (field) names.
-For most file formats, a data row must have length no longer than the header row,
+
+For most (non-varlen) file formats, a data row must have length no longer than the header row,
 and missing values are represented as "".
+
 For "varlen" files (files with variable row lengths), it is a little bit 
 different: the last header may correspond to 0, 1, 2, or more valuesin a data row.
 So the data row may be shorter (by one), equal to, or longer than the header row.
@@ -44,73 +45,72 @@ import ids
 import utils
 
 
-def read_csv_file(filename, fieldnames=None, varlen=False):
+def read_csv_file(filename, expected_fieldnames=None, varlen=False):
     """
-    Read CSV file and expected fieldnames; varlen if variable-length rows.
+    Read CSV file and check expected fieldnames; varlen if variable-length rows.
     """
 
-    print(filename)
+    # print("Reading CSV file:", filename)
     with open(filename) as file:
-        reader = csv.DictReader(file)
+        reader = csv.reader(file)
         rows = [row for row in reader]
-        # take care of case last fieldname is followed by a comma
-        while len(reader.fieldnames)>0 and ids.clean_id(reader.fieldnames[-1])=='':
-            reader.fieldnames.pop()
-            for row in rows:
-                row[None] = [row['']]+row.get(None,[])
+        fieldnames = rows[0]
+        rows = rows[1:]
+        
+        # gather, clean, and trim field names, eliminating blanks
+        fieldnames = [ids.clean_id(fieldname) for fieldname in fieldnames]
+        while len(fieldnames)>0 and fieldnames[-1]=='':
+            fieldnames.pop()
+        if len(set(fieldnames)) != len(fieldnames):
+            utils.myerror("Duplicate field name:"+fieldnames)
+
+        # data rows
+        row_dicts = []
         for row in rows:
-            if None in row and not varlen:
-                # raise Exception("Too many values in row:"+str(row))
-                utils.mywarning("Too many values in row:"+str(row))
-                del row[None]
-            for fieldname in reader.fieldnames:
-                clean_fieldname = ids.clean_id(fieldname)
-                if clean_fieldname != fieldname:
-                    if clean_fieldname in row:
-                        utils.mywarning("field name given twice:", clean_fieldname)
-                    row[clean_fieldname] = row[fieldname]
-                    del row[fieldname]
-                    fieldname = clean_fieldname
-                if row[fieldname] == None:
-                    row[fieldname] = ""
-                if isinstance(row[fieldname], str):
-                    row[fieldname] = ids.clean_id(row[fieldname])
+            row = ["" if item==None else ids.clean_id(item) for item in row]
+            while len(row)>0 and row[-1] == '':
+                row.pop()
+            if not varlen:
+                if len(row) > len(fieldnames):
+                    utils.mywarning("Ignoring extra values in row:"+str(row))
+                    row = row[:len(fieldnames)]
+                while len(row) < len(fieldnames):
+                    row.append("")
+            row_dict = {}
+            for (fieldname, value) in zip(fieldnames, row):
+                row_dict[fieldname] = value
             if varlen:
-                lastfieldname = ids.clean_id(reader.fieldnames[-1])
-                if row[lastfieldname] == None:
-                    value = ()
-                elif None not in row:
-                    value = (row[lastfieldname],)
-                else:
-                    while len(row[None])>0 and ids.clean_id(row[None][-1])=='':
-                        row[None].pop()
-                    value = [row[lastfieldname]] + [ids.clean_id(id) for id in row[None]]
-                    value = tuple(value)
-                    # Note: the previous line does *not* sort the ids into
-                    # order.  This will be needed if these ids represent selids in a
-                    # vote, but the order may be important for other uses.
-                    del row[None]
-                if value == ("",):
-                    value = ()
-                row[lastfieldname] = value
-        if fieldnames!=None:
-            fieldnames_expected = [ids.clean_id(id) for id in fieldnames]
-            fieldnames_present = [ids.clean_id(id) for id in reader.fieldnames]
-            if fieldnames_expected!=fieldnames_present:
+                if len(row) < len(fieldnames)-1:
+                    if len(row) > 0:
+                        utils.mywarning("Ignoring too-short row:"+str(row))
+                    continue
+                last_fieldname = fieldnames[-1]
+                last_value = tuple(row[len(fieldnames)-1:])
+                row_dict[last_fieldname] = last_value
+            row_dicts.append(row_dict)
+        if expected_fieldnames!=None:
+            expected_fieldnames = [ids.clean_id(id) for id in expected_fieldnames]
+            if expected_fieldnames != fieldnames:
                 utils.myerror("File {} has fieldnames {} instead of expected {}."
-                              .format(filename, fieldnames_present, fieldnames_expected))
-        return rows
+                              .format(filename, fieldnames, expected_fieldnames))
+        return row_dicts
 
 
 if __name__=="__main__":
 
     filename = "test_data/csv_readers_test_reg.csv"
-    print("Regular csv file:", filename)
+    print("Regular (non-varlen) csv file:", filename)
     for row in read_csv_file(filename):
-        print([(fn, row[fn]) for fn in sorted(row)])
+        for fieldname in sorted(row):
+            print("{}:'{}' ".format(fieldname, row[fieldname]), end='')
+        print()
 
     filename = "test_data/csv_readers_test_varlen.csv"
     print("Varlen csv file:", filename)
     for row in read_csv_file(filename, varlen=True):
-        print([(fn, row[fn]) for fn in sorted(row)])
+        for fieldname in sorted(row):
+            print("{}:'{}' ".format(fieldname, row[fieldname]), end='')
+        print()
+
+
     
