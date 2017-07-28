eb5e6bde589f3598d955c0d7389f7054a3a3c642
ron-rivest <rivest@mit.edu>
2017-07-28 12:44:31 -0700

    Add prototype code for testing scalability.

---
+++ 2017-code/test_scalability.py
@@ -0,0 +1,27 @@
+# test_scalability.py
+# Ronald L. Rivest with Huasyn Karimi
+
+import syn2
+
+def tester(se):
+
+    # copy code here from syn2.test; modify as needed
+
+def test_scale(k):
+
+    # start timer
+
+    se = syn2.SynElection()
+
+    # ... set parameters here based on k
+
+    # run "test"
+
+    # stop timer; print k and elapsed time
+
+
+for k in range(3, 8):
+    test_scale(k)
+
+    
+    
