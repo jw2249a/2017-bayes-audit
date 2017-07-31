6f18e26b8d98e6cdd8b551581d551ca5421b6fb6
ron-rivest <rivest@mit.edu>
2017-07-30 21:58:10 -0700

    Add some formatting to cli.py.

--- 2017-code/cli.py
+++ 2017-code/cli.py
@@ -33,26 +33,34 @@ def parse_args():
                         The name for this election of the subdirectory within the elections root directory.""")
     # All others are optional
     # First group sets parameters: election_name, elections_root, audit_seed
+
     parser.add_argument("--election_name", help="""
                         Human-readable name of the election.""",
                         default="TestElection")
+
     parser.add_argument("--elections_root", help="""The directory where the subdirectory for the
                         election is to be found.  Defaults to "./elections".""",
                         default="./elections")
+
     parser.add_argument("--audit_seed",
                         help="""Seed for the random number generator used for
                         auditing (arbitrary nonnegative integer). (If omitted, uses clock.)""")
-    ## v2:
+
     parser.add_argument("--read_specification", action="store_true", help="""
                         Read and check election specification.""")
+
     parser.add_argument("--read_reported", action="store_true", help="""
                         Read and check reported election data and results.""")
+
     parser.add_argument("--read_seed", action="store_true", help="""
                         Read audit seed.""")
+
     parser.add_argument("--make_orders", action="store_true", help="""
                         Make audit orders files.""")
+
     parser.add_argument("--read_audited", action="store_true", help="""
                         Read and check audited votes.""")
+
     parser.add_argument("--stage",
                         help="""Run stage STAGE of the audit (may specify "ALL").""")
     args = parser.parse_args()
@@ -73,20 +81,25 @@ def process_args(e, args):
     if args.read_specification:
         # print("read_specification")
         election_spec.get_election_spec(e)
+
     elif args.read_reported:
         print("read_reported")
         election_spec.get_election_spec(e)
         reported.get_election_data(e)
+
     elif args.read_seed:
         print("read_seed")
         election_spec.get_election_spec(e)
         reported.get_election_data(e)
         audit.get_audit_parameters(e, args)
+
     elif args.make_orders:
         print("make_orders")
         audit_orders.compute_audit_orders(e)
+
     elif args.read_audited:
         print("read_audited")
+
     elif args.stage:
         print("stage", args.stage)
         election_spec.get_election_spec(e)
