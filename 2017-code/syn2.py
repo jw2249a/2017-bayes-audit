8e6f03480fa151e116c277455465e7ca68126d77
ron-rivest <rivest@mit.edu>
2017-08-06 00:12:20 -0700

    Improve syn2.py for generating synthetic elections of type 2.

--- 2017-code/syn2.py
+++ 2017-code/syn2.py
@@ -13,6 +13,7 @@ import copy
 
 import syn
 import syn1
+import utils
 import write_csv
 
 
@@ -44,15 +45,42 @@ def process_spec(e, synpar, L):
 
     for (cid, pbcid, rv, av, num) in L:
 
-        e.cids.append(cid)
+        if cid not in e.cids:
+            e.cids.append(cid)
+            e.contest_type_c[cid] = "plurality"
+            e.winners_c[cid] = 1
+            e.write_ins_c[cid] = "no"
+            e.selids_c[cid] = {}
+            for selid in rv:
+                if selid not in e.selids_c[cid]:
+                    e.selids_c[cid][selid] = True
+            for selid in av:
+                if selid not in e.selids_c[cid]:
+                    e.selids_c[cid][selid] = True
+            e.ro_c[cid] = "FIX_ME"
 
-        e.pbcids.append(pbcid)
+        if pbcid not in e.pbcids:
+            e.pbcids.append(pbcid)
+            e.manager_p[pbcid] = "Nobody"
+            e.cvr_type_p[pbcid] = "CVR"
+            e.required_gid_p[pbcid] = ""
+            e.possible_gid_p[pbcid] = ""
+            e.bids_p[pbcid] = []
+            e.boxid_pb[pbcid] = {}
+            e.position_pb[pbcid] = {}
+            e.stamp_pb[pbcid] = {}
+            e.max_audit_rate_p[pbcid] = 40
+            e.shuffled_indices_p[pbcid] = "FIXME"
+            e.shuffled_bids_p[pbcid] = "FIXME"
 
-        for bidx in range(1, num+1):
-            bid = "bid{}".format(bidx)
+        for pos in range(1, num+1):
+            bid = "bid{}".format(pos)
             utils.nested_set(e.rv_cpb, [cid, pbcid, bid], rv)
             utils.nested_set(e.av_cpb, [cid, pbcid, bid], av)
-            
+            e.bids_p[pbcid].append(bid)
+            e.boxid_pb[pbcid][bid] = "box1"
+            e.position_pb[pbcid][bid] = pos
+            e.stamp_pb[pbcid][bid] = ""
 
 ##############################################################################
 ##
@@ -62,9 +90,13 @@ def generate_syn_type_2(e, args):
     synpar = copy.copy(args)
     # syn1.default_parameters(synpar)
 
-    generate_election_spec(e, synpar)
-    generate_reported(e, synpar)
-    generate_audit(e, synpar)
+    L = [ ("cid1", "pbcid1", ("Alice",), ("Bob,"), 3) ]
+
+    process_spec(e, synpar, L)
+
+    # generate_election_spec(e, synpar)
+    # generate_reported(e, synpar)
+    # generate_audit(e, synpar)
 
     debug = False
     if debug:
