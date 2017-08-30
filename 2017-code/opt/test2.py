ac9697d0ba7d807cbeb56bfa03a4036cc93a2d2e
Husayn Karimi <husayn@mit.edu>
2017-08-30 14:08:03 -0400

    merged

--- 2017-code/opt/test2.py
+++ 2017-code/opt/test2.py
@@ -17,6 +17,7 @@ We wish to minimize sum(x), subject to p(x) <= 0.05 say.
 """
 
 import numpy as np
+import operator 
 
 def compute_p(xs, us):
     zs = xs / us                       # normalize to zs in [0,1]
@@ -48,6 +49,44 @@ def main():
             xs[i] = max(0, xs[i] - step_size)
 
 main()
+
+def audit(contests, func, xs_c, us_c): 
+    """
+    1) set all contests measured risk limits to max 
+    2) pick contest at random at i=0
+    3) then pick contest based on highest measured risk 
+    """
+
+    fail_rates_c = dict() 
+    for contest in contests:
+        fail_rates_c[contest]=[1,1] # first num = num failures, second num = total draws
+
+    d = len(contests)
+    delta = 10
+    risk_limit = 0.05
+    success_move_prob = risk_limit / (1 - risk_limit)
+    steps = 1
+
+    while steps < 10000: # should this still be 10k?  
+        if steps==1:
+            choice = contests[np.random.choice(range(d))] # the contest 
+        else:
+            adjusted_failure = dict()
+            for contest in fail_rates_c:
+                adjusted_failure[contest] = float(fail_rates_c[contest][0])/float(fail_rates_c[contest][1])
+            sorted_fail_rates = sorted(adjusted_failure.items(), key=operator.itemgetter(1))
+            choice = sorted_fail_rates[-1][0] # the contest 
+        xs = xs_c[choice]
+        us = us_c[choice]
+        p = compute_p(xs, us)
+        failure = (np.random.uniform() < p)
+        if failure:
+            xs[i] = min(us[i], xs[i] + step_size)
+            fail_rates_c[choice][0]+=1
+        elif np.random.uniform() <= success_move_prob:
+            xs[i] = max(0, xs[i] - step_size)
+        fail_rates_c[choice][1]+=1
+        steps+=1
 
 
 
